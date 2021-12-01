7ab47ce673777202c38f437890ea0d8516826fed
keygenic <keygenicgithub@gmail.com>
2021-12-01 12:15:09 +0300

    data updated

--- src/components/categories-data/categories-data.js
+++ src/components/categories-data/categories-data.js
@@ -1,22 +1,54 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
+import { Card, CardContent, CardMedia, Pagination } from '@mui/material'
+import { Movies} from '../../movies.styles'
+import { GenreListMovies } from '../genre-lists/genre-lists.components'
 
 
-const data = (Component,MovieData) => {
-    const CategoriesData = () => {
-        const [data,setData] = useState([]);
-        useEffect(() => {
-            const getMoviesData = async() => {
-                const result = await axios.get(MovieData)
-                setData(result.data.results)
-            }
-            getMoviesData()
-        },[setData])
-        return(
-            <Component data = {data} />
-        )
+
+function CategoriesMovie({type,media,time}) {
+    const [data,setData] = useState([]);
+    const [page,setPage] = useState(1)
+    let api = `https://api.themoviedb.org/3/${type}/${media}/${time}?api_key=cc65c8449d31408a45621d9ff608f031&language=en-US&page=${page}`
+    useEffect(() => {
+        const getMoviesData = async() => {
+            const result = await axios.get(api)
+            setData(result.data.results)
+        }
+        getMoviesData()
+        return () => {
+            console.log('clean up')
+        }
+    },[page])
+    const handleChange = (event,page) => {
+        setPage(page)
     }
-    return CategoriesData;
+    return (
+        <Movies>
+            {
+                data.map(({title,id,vote_average,poster_path}) => {
+                    return    <Card sx={{ maxWidth: 345 }} key = {id}>
+                    <CardMedia
+                      component="img"
+                      alt="green iguana"
+                      height="200"
+                      image={`https://image.tmdb.org/t/p/original${poster_path}`}
+             
+                    />
+                    <CardContent>
+                      <h3>{title}</h3>
+                      <p className = 'rating'>
+                    {vote_average}
+                    </p>
+                    <GenreListMovies id = {id}/>
+                    </CardContent>
+                  </Card>
+                })
+            }
+            <Pagination count={11} page ={page} variant="outlined" color="primary" onChange={handleChange}  />
+        </Movies>
+        
+    )
 }
 
-export default data
+export default CategoriesMovie
